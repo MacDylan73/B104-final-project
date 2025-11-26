aa27e9da87186c76dd609963c08875ded85b0f84
Dylan Folscroft <dfolscroft805@gmail.com>
2025-11-25 23:44:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+"""
+B104 Final Project - Dylan Folscroft and Travis Godley
+11/25/2025
+
+Run this file, main.py, with the following files in the same folder:
+    - createGUI.py
+    - loading.py
+    - statistical_analysis.py
+    - YBRSS_COMBINED.csv
+"""
+
+
 import tkinter as tk
 from createGUI import gui
 
